3e8f0973bd0f0e284196d494ebb3340dc2e7edfa
KarolZawisza555 <karolzawisza555@gmail.com>
2023-01-28 19:38:19 +0100

    added routes

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,16 +1,27 @@
-
 const routes = [
   {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
-    ]
+    name: "index",
+    path: "/",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [{ path: "/", component: () => import("pages/IndexPage.vue") }]
   },
   {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue')
+    name: "agreementPage",
+    path: "/agreement",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [{ path: "", component: () => import("pages/AgreementPage.vue") }]
+  },
+  {
+    name: "done",
+    path: "/done",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [{ path: "", component: () => import("pages/DonePage.vue") }]
+  },
+  {
+    name: "error404",
+    path: "/:catchAll(.*)*",
+    component: () => import("pages/ErrorNotFound.vue")
   }
-]
+];
 
-export default routes
+export default routes;
